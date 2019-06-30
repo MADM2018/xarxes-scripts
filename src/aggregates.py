1c5a112231bfd82f8ccbddb1e68e06345d55d462
Reinier Mujica <reinier.mujica@gmail.com>
2019-06-30 19:09:11 +0200

    Moved aggregates to single file

---
+++ src/aggregates.py
@@ -0,0 +1,38 @@
+import os
+import bson
+
+from mongo_client import get_client
+
+def aggr_hashtags():
+    db = get_client()
+
+    hashtags = 0
+
+    for tweet in db.tweets.find():
+        # entities.hashtags
+
+        if 'entities' in tweet:
+            entities = tweet['entities']
+
+            if 'hashtags' in entities:
+                hashtags += len(tweet['entities']['hashtags'])
+
+    # insert
+    db.aggregates.update_one({'_id': bson.ObjectId(b'hashtags_num')}, {"$set": {"value": hashtags}}, upsert=True)
+
+def aggr_retweets():
+    db = get_client()
+
+    retweets = 0
+
+    for tweet in db.tweets.find():
+        # retweeted_status.retweet_count
+
+        if 'retweeted_status' in tweet:
+            retweeted_status = tweet['retweeted_status']
+
+            if 'retweet_count' in retweeted_status:
+                retweets += tweet['retweeted_status']['retweet_count']
+
+    # insert
+    db.aggregates.update_one({'_id': bson.ObjectId(b'retweets_num')}, {"$set": {"value": retweets}}, upsert=True)
---
+++ src/run_all.py
@@ -0,0 +1,8 @@
+from aggregates import aggr_hashtags, aggr_retweets
+
+def main():
+  aggr_hashtags()
+  aggr_retweets()
+
+if __name__ == "__main__":
+    main()
