b1d4079f918c7debf95ba087270e95b6810f8cd7
Reinier Mujica <reinier.mujica@gmail.com>
2019-07-03 21:35:24 +0200

    Fix int parse

--- src/profiles.py
+++ src/profiles.py
@@ -8,14 +8,14 @@ from aggregates import aggr_tweets_by_user_id_by_months
 def set_all_profiles_months_tweets():
     db = get_client()
 
-    months_acc = dict()
+    months_acc = {}
 
     for profile in db.profiles.find():
         if 'id' in profile:
             id = profile['id']
 
             months = aggr_tweets_by_user_id_by_months(id)
-            months_acc["{}".format(id)] = months
+            months_acc[str(int(id))] = months
 
     # insert
     db.aggregates.update_one({'_id': bson.ObjectId(b'tweets_month')}, {
